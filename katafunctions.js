147b99e4908cf3791c257f36dbad1594030eca44
Lucas Gordon <nextechcatapult@MacBook-Air-88.localdomain>
2018-06-28 15:20:41 -0400

    second commit

--- katafunctions.js
+++ katafunctions.js
@@ -1,15 +1,78 @@
-function KAdd() {
-
+function KAdd(x,y) {
+    //console.log(x+y);
+    return x+y;
 }
-function KMultiply() {
-
+function KMultiply(x,y) {
+    let z;
+    z = x;
+    for (var i=1; i<y; i++) {
+        z = KAdd(z,x);
+    }
+    //console.log(z);
+    return z;
 }
-function KPower() {
-
+function KPower(x,y) {
+    let z;
+    z = x;
+    for (var i=1; i<y; i++) {
+        z = KMultiply(z,x);
+    }
+    //console.log(z);
+    return z;
 }
-function KFactorial() {
-
+function KFactorial(x) {
+    for (var i = 1; i<x+3; i++) {
+        z = KMultiply(x,i);
+    }
+    console.log(z);
+    return z;
 }
-function KFibonacci() {
-    
+
+function KFactorial2(x) {
+    let z;
+    let y;
+    y = x - 1;
+    z = 0
+    for (x; x>=1; x--) {
+        z = z + KMultiply(x,y);
+        y--
+    }
+    console.log(z);
+    return z;
+}
+
+function KFactorial3(x) {
+    let y;
+    y = x;
+    x - 1;
+    for (x; x=0; x--) {
+        y = KMultiply(y,x);
+    }
+    console.log(y);
+    return y;
+}
+
+function KFactorial4(x) {
+    let y;
+    y = x;
+    for (var i=1; i<x; i++) {
+        y = KMultiply(i,y)
+    }
+    console.log(y);
+}
+
+function KFibonacci(input) {
+    let old = 0;
+    let last = 1;
+    let next;
+    for (var i=0; i<=input; i++) {
+        console.log('iteration: ' + i);
+        console.log('old: ' + old);
+        console.log('last: ' + last);
+        console.log('next: ' + next);
+        next = old + last;
+        old = last;
+        last = next;
+    }
+    console.log(next);
 }
